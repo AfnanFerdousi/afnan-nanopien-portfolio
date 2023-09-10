376fad3993dce3d748e30a189102b00047ad494c
AfnanFerdousi <afnanferdousi550@gmail.com>
2023-09-10 11:37:16 +0600

    resume part in progress

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -18,7 +18,28 @@ const Resume = () => {
                 </div>
 
                 {/* content part 1 */}
-                
+                <div className='grid grid-cols-2 lg:md:gap-x-28 py-8'>
+                    <div>
+                        <div className="flex items-center justify-between pb-6">
+                            <h2 className="flex flex-col font-montserrat">
+                                <span className="text-xl font-medium text-[#fff]">Afnan Ferdousi</span>
+                                <span className="text-md text-[#C4C4C4]">MERN Stack Developer</span>
+                            </h2>
+                            <h2 className="font-montserrat flex items-center gap-x-2">
+                                <span className="text-[#EF4765] text-6xl font-bold">2</span>
+                                <span className="flex flex-col text-[#C4C4C4] text-md">
+                                    Years
+                                    <span className="text-[#fff]">Experience </span>
+                                </span>
+                            </h2>
+                           
+                        </div>
+                        <hr className="w-42  border-t-2 border-gray-700" />
+                        <p className="pt-6">
+                            As a firm believer in continuous learning, I stay up-to-date with the latest advancements in web technologies. I am enthusiastic about taking on new challenges. I am enthusiastic about taking on new challenges. I am enthusiastic about taking on new challenges. I am enthusiastic about taking on new challenges.
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     );
