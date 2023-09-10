679edadc329ba4142ff451c801d6c35ebf2a43bb
AfnanFerdousi <afnanferdousi550@gmail.com>
2023-09-10 21:34:21 +0600

    resume done

--- next.config.js
+++ next.config.js
@@ -8,7 +8,16 @@ const nextConfig = {
       'i.pinimg.com',
       'i.redd.it',
       'img1.picmix.com',
-      'www.programming-hero.com', // Add 'www.programming-hero.com' to the domains array
+      'www.programming-hero.com',
+      'upload.wikimedia.org',
+      'cutewallpaper.org',
+      'seeklogo.com',
+      'static.macupdate.com',
+      'dl2.macupdate.com',
+      'assets.stickpng.com',
+      'static.wikia.nocookie.net',
+      'assets.vercel.com'
+      // Add 'cutewallpaper.org' to the domains array
     ],
   },
 };
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -31,9 +31,77 @@ const Resume = () => {
             width: 50, 
         },
     ];
+    const skills = [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "ReactJs",
+        "NextJs",
+        "NodeJs",
+        "ExpressJs",
+        "MongoDB",
+        "MySQL",
+        "Tailwind",
+        "Bootstrap",
+        "Mongoose",
+        "Typescript",
+        "Redux",
+        "Firebase",
+        "Prisma",        
+    ]
+    const tools = [
+        {
+            id: 1,
+            name: "VsCode",
+            logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/2048px-Visual_Studio_Code_1.35_icon.svg.png",
+        },
+        {
+            id: 2,
+            name: "Github",
+            logo: "https://cutewallpaper.org/24/github-logo-png/github-logo-icon-free-download-on-iconfinder.png",
+        },
+        {
+            id: 3,
+            name: "Figma",
+            logo: "https://upload.wikimedia.org/wikipedia/commons/a/ad/Figma-1-logo.png"
+        },
+        {
+            id: 4,
+            name: "Postman",
+            logo: "https://seeklogo.com/images/P/postman-logo-0087CA0D15-seeklogo.com.png"
+        },
+        {
+            id: 5,
+            name: "MongoDb Compass",
+            logo: "https://static.macupdate.com/products/62461/m/mongodb-compass-logo.png?v=1663508519"
+        },
+        {
+            id: 6,
+            name: "PgAdmin 4",
+            logo: "https://dl2.macupdate.com/images/icons256/60968.png?time=1663919426",
+        },
+        {
+            id: 7,
+            name: "Trello",
+            logo: "https://assets.stickpng.com/images/58482beecef1014c0b5e4a36.png"
+        },
+        {
+            id: 8,
+            name: "Vercel",
+            logo: "https://assets.vercel.com/image/upload/front/favicon/vercel/152x152.png"
+        },
+        {
+            id: 9,
+            name: "Swagger",
+            logo: "https://upload.wikimedia.org/wikipedia/commons/a/ab/Swagger-logo.png"
+        }
+    ]
 
     return (
-        <div className="px-8 py-40">
+        <div
+            data-aos="fade-up"
+            data-aos-duration="2000"
+            className="px-10 py-40">
             <div className="rounded-xl px-6 py-12" style={{ background: 'rgba(217, 217, 217, 0.1)' }}>
                 {/* header */}
                 <div className="flex items-center justify-between">
@@ -79,6 +147,48 @@ const Resume = () => {
                             </div>
                         ))}
                     </div>
+                </div>
+
+                {/* content part 2 */}
+                <div className="grid grid-cols-2 lg:md:gap-x-28 py-8">
+                    <div>
+                        <h2 className='text-xl font-medium text-[#fff] font-montserrat '>Skills</h2>
+                        <hr className="w-42  border-t-2 border-gray-700 mb-6 mt-4" />
+                        <div className="grid lg:md:grid-cols-5 grid-cols-4 gap-4 items-center justify-between pb-6">
+                            {
+                                skills.map((skill) => (
+                                    <div key={skill} style={{ background: 'rgba(255, 255, 255, 0.12)' }} className='rounded-lg'>
+                                        <h2 className="text-md font-montserrat font-medium text-[#fff] p-2 text-center">{skill}</h2>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div>
+                    <div>
+                        <h2 className='text-xl font-medium text-[#fff] font-montserrat '>Tools I use every day</h2>
+                        <hr className="w-42  border-t-2 border-gray-700 mb-6 mt-4" />
+                        <div className='grid lg:md:grid-cols-6 gap-4'>
+                            {tools.map((tool) => (
+                                <div key={tool?.id} className='rounded-lg'>
+                                    <Image
+                                        src={tool?.logo}
+                                        alt={tool?.name}
+                                        width={50}
+                                        height={50}
+                                    />
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+
+                </div>
+
+                {/* content part 3 */}
+                <div>
+                    <h2 className='text-xl font-medium text-[#fff] font-montserrat '>Freelance clients</h2>
+                    <hr className="w-42  border-t-2 border-gray-700 mb-6 mt-4" />
+
+                    <h2 className='text-2xl font-Raleway  '>Loading...</h2>
                 </div>
             </div>
         </div>
