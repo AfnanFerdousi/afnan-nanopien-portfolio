f44eae3911ee5951095b0849366a6254a90331d8
AfnanFerdousi <afnanferdousi550@gmail.com>
2023-09-10 23:41:32 +0600

    projects done

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -52,12 +52,14 @@ const Projects = () => {
     const cardSizes = ['w-1/2', 'w-1/3', 'w-1/4', 'w-1/3', 'w-1/4'];
 
 return (
-        <div>
+    <div
+        data-aos="fade-up"
+        data-aos-duration="3000">
             <h2 className='text-5xl font-montserrat font-semibold text-[#fff] text-center'>
                 Let’s check my projects
             </h2>
-            <p className='text-md text-[#C4C4C4] text-center mt-4'>As a firm believer in continuous learning, I stay up-to-date with the latest advancements in web technologies.</p>
-            <div className="flex flex-wrap justify-center">
+            <p className='text-md text-[#C4C4C4] text-center mt-4 w-[40%] mb-8 mx-auto'>As a firm believer in continuous learning, I stay up-to-date with the latest advancements in web technologies.</p>
+            <div className="flex flex-wrap justify-center pb-8">
                 {projects.map((project, index) => (
                     <div key={project.id} className={`mx-2 py-4 h-full cursor-pointer ${cardSizes[index]}`} >
                         <div className="relative bg-gray-700 shadow-lg rounded-lg overflow-hidden group transition-transform transform scale-100 hover:scale-105 hover:shadow hover:bg-[#333]">
@@ -97,7 +99,13 @@ return (
                         </div>
                     </div>
                 ))}
-            </div>
+        </div>
+        <button
+            className='py-4 px-8 text-[15px] font-medium rounded-lg text-[#E0DEDE] border-[1px] border-[#E0DEDE] mx-auto flex justify-center transition duration-300 ease-in-out hover:bg-gray-500 hover:text-white hover:scale-105'
+        >
+            Show All
+        </button>
+
         </div>
     );
 };
